d54b55bd8f9ea7c2876b3c95efdf26b87db1adad
TSI-ashishsharma <sharma.a@t-systems.com>
2022-08-05 11:03:11 +0530

    change parameter in array for storage

--- lib/Service/MailSender.php
+++ lib/Service/MailSender.php
@@ -159,11 +159,11 @@ class MailSender {
 			// Message no quota
 			$this->provider->writeStorageNoQuota($emailTemplate, $storageInfo);
 			return false;
-		} elseif ($storageInfo['relative'] < 90) {
+		} elseif ($storageInfo['usage_relative'] < 90) {
 			// Message quota but less than 90% used
 			$this->provider->writeStorageSpaceLeft($emailTemplate, $storageInfo);
 			return false;
-		} elseif ($storageInfo['relative'] < 99) {
+		} elseif ($storageInfo['usage_relative'] < 99) {
 			$this->provider->writeStorageWarning($emailTemplate, $storageInfo);
 			return true;
 		} else {
