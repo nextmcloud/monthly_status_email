fb4562f82a91cab448b26b1561170d73532cc817
Mauro <Mauro-Efisio.Mura@t-systems.com>
2024-04-15 17:04:16 +0200

    added magentacloud customizations

--- lib/Settings/PersonalSettings.php
+++ lib/Settings/PersonalSettings.php
@@ -65,7 +65,7 @@ class PersonalSettings implements ISettings {
 		if (version_compare($this->ncVersion, '23.0.0', '<')) {
 			return 'activity';
 		}
-		return 'notifications';
+		return 'account';
 	}
 
 	/**
